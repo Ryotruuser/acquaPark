fe0e6794cb3bc1a6f2655549a389edee371aff03
Ryotruuser <ryonantruuser@gmail.com>
2025-03-10 20:22:50 -0300

    criado toogle de expandir as fotos na galeria fazendo os comentarios sumirem ate que seja cliclado novamente

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const sectionComentarios = document.getElementById("galeria__comentarios");
+const sectionFotos = document.getElementById("galeria__fotos");
+const galeriaFotos = document.getElementsByClassName("galeria__img");
+//
+const expandirComentariosOff = document.getElementById("galeria__expandir_off");
+const expandirComentariosOn = document.getElementById("galeria__expandir_om");
+
+comentariosOff = () => {
+    
+    if(galeriaFotos[0].classList.length == 1){
+        sectionComentarios.style.display = "none";
+        sectionFotos.style.width = "100%"
+        for (let i = 0; i <= 18; i++) {
+            galeriaFotos[i].classList.add("galeria__expandir_foto");
+        }
+    }else{
+        sectionFotos.style.width = "70vw";
+        sectionComentarios.style.display = "flex";
+        galeriaFotos[0].classList.remove("galeria__expandir_foto");
+    }
+   
+}

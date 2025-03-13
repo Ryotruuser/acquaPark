96bfb67617876fa0775ccd599fef9451bfed09f1
Ryotruuser <ryonantruuser@gmail.com>
2025-03-12 21:14:29 -0300

    feita base para inicio da mobilizacao por assim dizer

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ const galeriaFotos = document.getElementsByClassName("galeria__img");
 const expandirComentariosOff = document.getElementById("galeria__expandir_off");
 const expandirComentariosOn = document.getElementById("galeria__expandir_om");
 
-comentariosOff = () => {
+expandir = () => {
     
     if(galeriaFotos[0].classList.length == 1){
         sectionComentarios.style.display = "none";
@@ -14,9 +14,20 @@ comentariosOff = () => {
             galeriaFotos[i].classList.add("galeria__expandir_foto");
         }
     }else{
-        sectionFotos.style.width = "70vw";
+        sectionFotos.style.width = "65vw";
         sectionComentarios.style.display = "flex";
         galeriaFotos[0].classList.remove("galeria__expandir_foto");
     }
    
 }
+
+// ativarComentario = () => {
+//     if(sectionComentarios.style.display == "none"){
+//         sectionFotos.style.width = "50%"
+//         sectionComentarios.style.display = "block";
+//     }else{
+//         sectionFotos.style.width = "100%"
+//         sectionComentarios.style.display = "none";
+//     }
+
+// }
